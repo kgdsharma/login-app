635e2c7b2f2e858e4b8c535e20fde0ae2e04aba7
Krishna Sharma <kgdsharma@gmail.com>
2020-06-28 17:09:28 -0400

    updated proxy config

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";  
 module.exports = function (app) {
   app.use(
-    ["/api"],
+    ["/api/*"],
     createProxyMiddleware({
       target: API_BASE_URL
     })
