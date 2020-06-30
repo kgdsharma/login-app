94c3cbb95e41afb40c56ad982398c6d1e777edfd
Krishna Sharma <kgdsharma@gmail.com>
2020-06-30 00:51:42 -0400

    updated documentation for proxy setting in local vs prod

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,3 +7,25 @@ module.exports = function (app) {
     })
   );
 };
+
+//################################################################################################
+//NOTE: Everything started with '/api' gets appended to target. e.g if an api call is `/api/auth` 
+// that will become `http://localhost:5000/api/auth` while proxying.
+// while for production in static.json the proxy configuration everything after '/api/' gets 
+// appended. Therefore, add the `/api` at the end of origin. Example below. The `/api` is being 
+// appended to origin  `https://${API_APP_NAME}.herokuapp.com`
+
+// static.json <at the root of the project>.
+//{
+//   "root":"build/",
+//   "routes": {
+//       "/**": "index.html"
+//     },
+//   "proxies": {
+//     "/api/*": {
+//       "origin": "https://${API_APP_NAME}.herokuapp.com/api"
+//     }
+//   }
+// }
+//This configuration will work when working on localhost and after deploying the app to Heroku.
+//##################################################################################################
